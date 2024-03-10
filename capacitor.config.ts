7deb3d4f99077e4e249333b10694689f758120a2
cristofer <ml01137152@uls.edu.sv>
2024-03-09 22:40:17 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jardin.lunar',
+  appName: 'jardin-lunar',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
